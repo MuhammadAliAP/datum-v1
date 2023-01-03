43855cc274834a9052597ca4df7a744636c6e6d3
unknown <muhammedaliap786@gmail.com>
2023-01-03 13:26:00 +0530

    datum v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ViewComponent } from './view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path:'',redirectTo:'home',pathMatch:'full'
+  // },
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'view/:id',component:ViewComponent
+  },
+  {
+    path:'signin',component:SignInComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ViewComponent } from './view/view.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewComponent,
+    SignInComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
